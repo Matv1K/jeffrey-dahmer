dfec540113c457e5055c2895a6df178ee9650cf3
Matv1K <balahonovmatvej@gmail.com>
2024-06-30 03:24:16 +0300

    Work on contact page. Test rarely-used HTML tags

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,66 @@
+import "./Contact.scss";
+
+import { Figure } from "../../img";
+
 const Contact = () => {
   return (
     <div>
-      <h1>Contact page</h1>
+      <span>
+        This is an <b>example</b> of bold text which is not important. B
+      </span>
+
+      <hr />
+
+      <span>
+        This is an <strong>example</strong> of bold text which is not important.
+        STRONG
+      </span>
+
+      <hr />
+
+      <span>
+        This is <abbr>W2S</abbr>. ABBR
+      </span>
+
+      <hr />
+
+      <span>
+        This is an <em>example</em> of some italic text that will be read by
+        screenreader. EM
+      </span>
+
+      <form className="form">
+        <h4 contentEditable>
+          Example of fieldset and legend tags. This heading has contenteditable
+          attribute
+        </h4>
+
+        <fieldset>
+          <legend>Required</legend>
+          <input placeholder="Enter your name..." type="text" />
+          <input placeholder="Enter your password..." type="password" />
+        </fieldset>
+
+        <input placeholder="Enter your email..." type="email" />
+      </form>
+
+      <hr />
+
+      <h4 draggable>
+        Example of figure and figcaption tags. This heading has draggable
+        attribute
+      </h4>
+
+      <figure className="figure">
+        <img width="30%" alt="Weird figure" src={Figure} />
+        <figcaption>That is some weird figure</figcaption>
+      </figure>
+
+      <hr />
+
+      <p>
+        This is <kbd>Ctrl</kbd> key
+      </p>
     </div>
   );
 };
